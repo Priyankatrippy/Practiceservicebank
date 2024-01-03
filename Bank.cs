024a060f601d94b0216e1d5d888ac4a78dbc5aa9
priyanka <priyankatripathy81@gmail.com>
2024-01-03 16:36:35 +0530

    Adding all the cs , dll,pdb,csproj files

---
+++ Bank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBank
+{
+    internal class Bank
+    {
+        public void savings() { Console.WriteLine("Savings"); }
+        public void Current() { Console.WriteLine("Current"); }
+        public void Recurring() { Console.WriteLine("Recurring"); }
+
+    }
+}
